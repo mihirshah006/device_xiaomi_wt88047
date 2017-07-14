5b434db3453b1b4325f66107137efe66456d4d8c
mihirshah006 <smihirj9@gmail.com>
2017-07-14 17:10:59 +0530

    Update and rename zos_wt88047.mk to carbon_wt88047.mk (#1)

--- carbon_wt88047.mk
+++ carbon_wt88047.mk
@@ -15,8 +15,8 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Zephyr stuff.
-$(call inherit-product, vendor/zos/config/common_full_phone.mk)
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 # Inherit from wt88047 device
 $(call inherit-product, device/wingtech/wt88047/device.mk)
@@ -28,7 +28,7 @@ TARGET_BOARD_PLATFORM_VARIANT := msm8916
 BOARD_VENDOR := wingtech
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := wt88047
-PRODUCT_NAME := zos_wt88047
+PRODUCT_NAME := carbon_wt88047
 PRODUCT_MANUFACTURER := Wingtech
 PRODUCT_MODEL := Redmi 2
 
